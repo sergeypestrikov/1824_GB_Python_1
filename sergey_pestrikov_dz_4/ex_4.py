69a3ce9366ad9cd9f05e21b318359ba226daa57e
Sergey Pestrikov <sergey_pestrikov@mail.ru>
2021-10-18 19:39:52 +0300

    Добавлены сами задачи

--- sergey_pestrikov_dz_4/ex_4.py
+++ sergey_pestrikov_dz_4/ex_4.py
@@ -2,3 +2,7 @@ from utils import currency_rates
 
 print(currency_rates('USD'))
 print(currency_rates('EUR'))
+
+#Написать свой модуль utils и перенести в него функцию currency_rates() из предыдущего задания.
+# Создать скрипт, в котором импортировать этот модуль и выполнить несколько вызовов функции currency_rates().
+# Убедиться, что ничего лишнего не происходит.
